dd35ac5da44f6f3f43976bc592dfcef5b222405d
Denis Sazhaev <denis.sazhaev@block64.com>
2021-12-19 12:32:41 -0700

    Clean up code

--- aioedgeos/api.py
+++ aioedgeos/api.py
@@ -1,12 +1,10 @@
 import asyncio
-from aiohttp import ClientSession,CookieJar,Fingerprint,ClientTimeout,WSMsgType
+from aiohttp import ClientSession, CookieJar, ClientTimeout, WSMsgType
 
-from binascii import a2b_hex
-from urllib.parse import urlparse
 import json
 from time import time
 
-from contextlib import AsyncExitStack, asynccontextmanager, suppress
+from contextlib import AsyncExitStack, suppress
 import logging
 
 logger = logging.getLogger('aioedgeos')
@@ -14,6 +12,8 @@ logger = logging.getLogger('aioedgeos')
 '''
 Helper to find dhcp mappings
 '''
+
+
 def find_subkey(data, keyname):
     if not isinstance(data, dict):
         return
@@ -24,24 +24,31 @@ def find_subkey(data, keyname):
             yield from find_subkey(value, keyname)
     return
 
+
 '''
 Return the object formatted in a way preferred by EdgeOS
 '''
+
+
 def json_compact(obj):
-    return json.dumps(obj,separators=(',', ':'))
+    return json.dumps(obj, separators=(',', ':'))
+
 
 '''
 Format for statd
 '''
+
+
 def as_statd_string(obj):
     text = json_compact(obj)
-    return "{}\n{}".format(len(text),text)
+    return "{}\n{}".format(len(text), text)
+
 
 class TaskEvery:
     def __init__(self, call, *args, interval=0, sync_once=None, offset=0, **kwargs):
         if sync_once and offset:
             raise ValueError("Can't set sync_once and offset together")
-        if sync_once == None and not offset:
+        if sync_once is None and not offset:
             sync_once = True
         if not call:
             raise ValueError("Call can't be None")
@@ -58,17 +65,17 @@ class TaskEvery:
     async def run(self):
         try:
             await self.call(*self.args, **self.kwargs)
-        except Exception as e:
+        except Exception:
             logging.exception(f"Run of {self.call.__name__} raised exception")
             raise
 
     async def run_every(self):
         while 1:
             ''' interval time - time elapsed + any offset '''
-            sleep_for = self.interval-(time()-self.last)
+            sleep_for = self.interval - (time() - self.last)
             await asyncio.sleep(sleep_for)
             self.last = time()
-            #logging.debug("Periodic run of {}".format(self.call.__name__))
+            # logging.debug("Periodic run of {}".format(self.call.__name__))
             await self.run()
 
     async def __aenter__(self):
@@ -85,20 +92,27 @@ class TaskEvery:
             self.task.cancel()
             await self.task
 
+
 '''
 Given an edgeos object test for failed login and relogin every interval
 seconds
 '''
+
+
 async def stay_logged_in(edgeos):
     with suppress():
         if not await edgeos.is_logged_in():
             await edgeos.login()
 
+
 '''
 Given a WebSocket send nonstandard ping every interval seconds ( default 30)
 '''
+
+
 async def ws_ping(ws):
     await ws.send_str('{"CLIENT_PING"}')
+
 
 class EdgeOS:
     username = None
@@ -110,15 +124,16 @@ class EdgeOS:
     stack = None
 
     def __init__(self, username, password, url, ssl=None, session_id=None):
+        self.sysconfig = None
         self.username = username
         self.password = password
         self.url = url
         self.ssl = ssl
         self.session = None
         self.session_id = None
-        self.headers = { 'Content-type': 'application/json' }
-        self.cookies = { }
-        self.sysdata = { 'ping-data': {} }
+        self.headers = {'Content-type': 'application/json'}
+        self.cookies = {}
+        self.sysdata = {'ping-data': {}}
         if session_id:
             self.session_id = session_id
             self.cookies['beaker.session.id'] = session_id
@@ -129,12 +144,12 @@ class EdgeOS:
         if not self.session:
             # If no session provided create our own, push on stack to make sure it's cleaned up later
             self.session = await self.stack.enter_async_context(
-                    ClientSession(cookie_jar=CookieJar(unsafe=True), raise_for_status=True, timeout=ClientTimeout(15)))
+                ClientSession(cookie_jar=CookieJar(unsafe=True), raise_for_status=True, timeout=ClientTimeout(15)))
         ''' Start login loop, won't return until first run is complete '''
         await self.stack.enter_async_context(TaskEvery(stay_logged_in, self, interval=300))
 
     async def close(self):
-        for key,value in self.tasks.items():
+        for key, value in self.tasks.items():
             try:
                 value.cancel()
                 await value
@@ -161,19 +176,19 @@ class EdgeOS:
 
     async def is_logged_in(self):
         with suppress():
-            if await self.sys_info() != None:
+            if await self.sys_info() is not None:
                 return True
         return False
 
     async def login(self):
         if self.username and self.password:
             async with self.session.post(f'{self.url}',
-                                         data={'username':self.username, 'password': self.password}, 
-                                         ssl=self.ssl) as resp:
+                                         data={'username': self.username, 'password': self.password},
+                                         ssl=self.ssl):
                 try:
                     s_id = self.session.cookie_jar.filter_cookies(self.url)['beaker.session.id'].value
                     token = self.session.cookie_jar.filter_cookies(self.url)['X-CSRF-TOKEN'].value
-                    self.headers = { 'X-CSRF-TOKEN': token }
+                    self.headers = {'X-CSRF-TOKEN': token}
                     self.session_id = s_id
                     self.cookies['beaker.session.id'] = s_id
                     logger.debug("New seesion ending in {}".format(s_id[:4]))
@@ -183,17 +198,16 @@ class EdgeOS:
 
     async def data(self, data_type):
         try:
-            result = None
             async with self.session.get(f"{self.url}/api/edge/data.json?data={data_type}",
-                                        ssl=self.ssl, 
-                                        headers=self.headers, 
-                                        raise_for_status=False, 
+                                        ssl=self.ssl,
+                                        headers=self.headers,
+                                        raise_for_status=False,
                                         cookies=self.cookies) as resp:
                 if resp.status != 200:
                     return None
                 result = await resp.json()
                 if result.get('success', 0):
-                    self.sysdata[data_type] = result.get('output',None)
+                    self.sysdata[data_type] = result.get('output', None)
                 else:
                     return None
             return result
@@ -223,9 +237,6 @@ class EdgeOS:
     async def routes(self):
         return await self.data('routes')
 
-    async def sys_info(self):
-        return await self.data('sys_info')
-
     async def config(self):
         async with self.session.get(f"{self.url}/api/edge/get.json",
                                     ssl=self.ssl,
@@ -241,21 +252,21 @@ class EdgeOS:
 
     async def ping(self, target='1.1.1.1', count=3, size=100):
         ret = ''
-        init = {'SUBSCRIBE': [{ 'name': 'ping-feed', 'sub_id': f'ping-{target}', 'target': target, 'count': count, 'size': size }]}
+        init = {'SUBSCRIBE': [
+            {'name': 'ping-feed', 'sub_id': f'ping-{target}', 'target': target, 'count': count, 'size': size}]}
         async for payload in self._ws(init=init, keepalive=False, timeout=15):
             ret += payload[f'ping-{target}']
             if f"--- {target} ping statistics ---" in ret:
                 self.sysdata['pinglast'] = ret
                 return self.process_ping(ret, target)
-                
 
     def process_ping(self, output, target):
-        data = { target: { 'time': time() }}
+        data = {target: {'time': time()}}
         for line in output.splitlines():
             if 'packets transmitted' in line:
                 sent, _, _, recv, *_ = line.split()
                 data[target]['sent'] = int(sent)
-                data[target]['lost'] = int(sent)-int(recv)
+                data[target]['lost'] = int(sent) - int(recv)
                 continue
             if 'min/avg/max/mdev' in line:
                 _, dat = line.split("=")
@@ -265,7 +276,7 @@ class EdgeOS:
                 data[target]['avg'] = pdat[1]
                 data[target]['max'] = pdat[2]
                 data[target]['mdev'] = pdat[3]
-                        
+
         self.sysdata['ping-data'].update(data)
         return data
 
@@ -279,29 +290,24 @@ class EdgeOS:
                 await self.ping(target, **kwargs)
                 await asyncio.sleep(interval)
 
-    async def background_stats(self, subs=["export", "discover","interfaces","system-stats","num-routes","config-change", "users"]):
-        # self- registering task
-        task = asyncio.current_task()
-        if task:
-            await self.add_task('stats', task)
-        with suppress(asyncio.CancelledError):
-            async for payload in self.stats(subs):
-                pass
-
-    async def stats(self, subs=["export", "discover","interfaces","system-stats","num-routes","config-change", "users"], reload_on_change=True):
-        if reload_on_change and ( not 'config-change' in subs ):
+    async def stats(self,
+                    subs=None,
+                    reload_on_change=True):
+        if subs is None:
+            subs = ["export", "discover", "interfaces", "system-stats", "num-routes", "config-change", "users"]
+        if reload_on_change and 'config-change' not in subs:
             subs.append('config-change')
-        init = {'SUBSCRIBE': [{'name': x } for x in subs]}
+        init = {'SUBSCRIBE': [{'name': x} for x in subs]}
         async for payload in self._ws(init=init):
             with suppress(KeyError):
-                if reload_on_change and 'config-change' in payload and payload.get('config-change',[])['commit'] == 'ended':
+                if reload_on_change and 'config-change' in payload \
+                        and payload.get('config-change', [])['commit'] == 'ended':
                     logger.debug("Detected config change, refreshing config cache")
                     await self.config()
             yield payload
 
     async def _ws(self, init, keepalive=True, timeout=30):
-        pinger = None
-        foo = { 'UNSUBSCRIBE': [] }
+        foo = {'UNSUBSCRIBE': []}
         foo.update(init)
 
         while True:
@@ -319,22 +325,26 @@ class EdgeOS:
 
                 async with AsyncExitStack() as stack:
 
-                    ws = await stack.enter_async_context(self.session.ws_connect(f"{self.url}/ws/stats", headers=self.headers, origin=self.url, ssl=self.ssl))
-                    pinger = await stack.enter_async_context(TaskEvery(ws_ping, ws, interval=30,sync_once=False))
+                    ws = await stack.enter_async_context(
+                        self.session.ws_connect(f"{self.url}/ws/stats", headers=self.headers, origin=self.url,
+                                                ssl=self.ssl))
+                    await stack.enter_async_context(TaskEvery(ws_ping, ws, interval=30, sync_once=False))
 
-                    foo.update({'SESSION_ID': self.session_id })
+                    foo.update({'SESSION_ID': self.session_id})
                     await ws.send_str(as_statd_string(foo))
                     data = ''
                     while True:
-                        msg =  await asyncio.wait_for(ws.receive(), timeout)
+                        msg = await asyncio.wait_for(ws.receive(), timeout)
 
                         if msg.type != WSMsgType.TEXT:
-                            logging.debug(f"got non text websocket data {msg.data!r} this probbaly means the socket was closed so let's start a fresh one")
+                            logging.debug(
+                                f"got non text websocket data {msg.data!r} "
+                                f"this probbaly means the socket was closed so let's start a fresh one")
                             break
-    
+
                         data += msg.data
-    
-                        temp1, temp2 = data.split('\n',1)
+
+                        temp1, temp2 = data.split('\n', 1)
                         data_len = int(temp1)
                         '''
                         If the payload is larger than the data len, process until it's less
@@ -350,14 +360,13 @@ class EdgeOS:
                             Strip off the processed data and leave the next for another round
                             '''
                             data = temp2[data_len:]
-                            if len(data) < 4: break
-                            temp1, temp2 = data.split('\n',1)
+                            if len(data) < 4:
+                                break
+                            temp1, temp2 = data.split('\n', 1)
                             data_len = int(temp1)
-            except asyncio.CancelledError as err:
+            except asyncio.CancelledError:
                 return
             except Exception as err:
                 logger.debug(f"websocket loop raised {err!r}, ignoring")
                 if not keepalive:
                     return
-
-
